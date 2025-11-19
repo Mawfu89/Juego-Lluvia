8118c68430ffd359c4e96a8b97d067eeefe74a29
Matías Arancibia Becerra <164715599+Mawfu89@users.noreply.github.com>
2025-11-19 20:51:43 -0300

    Delete JuegoLluvia directory

--- JuegoLluvia/core/src/main/java/puppy/code/Activable.java
+++
@@ -1,9 +0,0 @@
-package puppy.code;
-
-/**
- * Cualquier objeto del juego que pueda activarse sobre el jugador (Tarro),
- * debe implementar esta interfaz.
- */
-public interface Activable {
-    void activar(Tarro tarro);
-}
